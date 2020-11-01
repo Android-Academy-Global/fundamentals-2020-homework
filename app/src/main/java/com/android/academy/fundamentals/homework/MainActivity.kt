f22e8cd77c924c0fb9421dba610179280174ef11
Pavel Shchahelski <p_shchahelski@flo.health>
2020-11-01 13:50:19 +0300

    Initial project setup.

---
+++ app/src/main/java/com/android/academy/fundamentals/homework/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.academy.fundamentals.homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
