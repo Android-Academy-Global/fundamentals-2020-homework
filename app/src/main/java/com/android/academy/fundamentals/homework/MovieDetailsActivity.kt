2be7d43be46359e920c4402b9a829d4a7a82b94c
rHlushan <ruslan.hlushen@gmail.com>
2020-11-15 22:54:34 +0300

    homework 2

--- app/src/main/java/com/android/academy/fundamentals/homework/MovieDetailsActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/homework/MovieDetailsActivity.kt
@@ -3,9 +3,9 @@ package com.android.academy.fundamentals.homework
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_details)
     }
 }
