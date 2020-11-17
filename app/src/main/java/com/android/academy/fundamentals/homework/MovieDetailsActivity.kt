756959b363f2b060361deecfe2b967562ac299b9
Pavel Shchahelski <p_shchahelski@flo.health>
2020-11-17 23:33:40 +0300

    Add dimens for size and spacing.

--- app/src/main/java/com/android/academy/fundamentals/homework/MovieDetailsActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/homework/MovieDetailsActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MovieDetailsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
