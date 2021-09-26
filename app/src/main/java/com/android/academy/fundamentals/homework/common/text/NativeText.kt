b9038c479ad327cdccc5ea214a2225900a15d3ac
vadzimv <vysotski.vadim@gmail.com>
2021-09-26 14:28:45 +0300

    added native text

---
+++ app/src/main/java/com/android/academy/fundamentals/homework/common/text/NativeText.kt
@@ -0,0 +1,29 @@
+package com.android.academy.fundamentals.homework.common.text
+
+import android.content.Context
+import androidx.annotation.PluralsRes
+import androidx.annotation.StringRes
+
+sealed class NativeText {
+    data class Simple(val text: String) : NativeText()
+    data class Resource(@StringRes val id: Int) : NativeText()
+    data class Plural(@PluralsRes val id: Int, val number: Int, val args: List<Any>) : NativeText()
+    data class Arguments(@StringRes val id: Int, val args: List<Any>) : NativeText()
+    data class Multi(val text: List<NativeText>) : NativeText()
+}
+
+fun NativeText.toCharSequence(context: Context): CharSequence {
+    return when (this) {
+        is NativeText.Arguments -> context.getString(id, *args.toTypedArray())
+        is NativeText.Multi -> {
+            val builder = StringBuilder()
+            for (t in text) {
+                builder.append(t.toCharSequence(context))
+            }
+            builder.toString()
+        }
+        is NativeText.Plural -> context.resources.getQuantityString(id, number, *args.toTypedArray())
+        is NativeText.Resource -> context.getString(id)
+        is NativeText.Simple -> text
+    }
+}
