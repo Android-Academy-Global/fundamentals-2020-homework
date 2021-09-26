e030c6aeeec5dc41d1816e202c57709259288800
vadzimv <vysotski.vadim@gmail.com>
2021-09-26 11:53:48 +0300

    added example test

---
+++ app/src/test/java/com/android/academy/fundamentals/homework/ExampleTest.kt
@@ -0,0 +1,13 @@
+package com.android.academy.fundamentals.homework
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+
+class ExampleTest {
+    @Test
+    fun `test plus operator`() {
+        val result = 2 + 2
+        assertEquals(4, result)
+    }
+}
