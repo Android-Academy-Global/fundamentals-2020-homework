ae84f1d1408b5ae3498dc2b00723360562f64fb1
vadzimv <vysotski.vadim@gmail.com>
2021-09-26 13:40:50 +0300

    step 2: Add tests for MoviesListViewModelImpl. added fail scenario

--- app/src/test/java/com/android/academy/fundamentals/homework/presentation/features/movies/viewmodel/MoviesListViewModelImplTest.kt
+++ app/src/test/java/com/android/academy/fundamentals/homework/presentation/features/movies/viewmodel/MoviesListViewModelImplTest.kt
@@ -1,5 +1,6 @@
 package com.android.academy.fundamentals.homework.presentation.features.movies.viewmodel
 
+import com.android.academy.fundamentals.homework.common.model.Failure
 import com.android.academy.fundamentals.homework.common.model.Result
 import com.android.academy.fundamentals.homework.common.model.Success
 import com.android.academy.fundamentals.homework.domain.MovieRepository
@@ -10,6 +11,7 @@ import com.android.academy.fundamentals.homework.utils.viewModelTestingRules
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class MoviesListViewModelImplTest {
     @get:Rule
@@ -52,7 +54,8 @@ class MoviesListViewModelImplTest {
                 imageUrl = null
             ),
         )
-        val repository = StubMovieRepository(movies = movies)
+        val repository = StubMovieRepository()
+        repository.setResult(movies)
 
         val viewModel = MoviesListViewModelImpl(repository, MoviesListItemMapper())
 
@@ -97,10 +100,33 @@ class MoviesListViewModelImplTest {
             )
     }
 
-    internal class StubMovieRepository(
-        val movies: List<Movie> = emptyList()
-    ) : MovieRepository {
-        override suspend fun loadMovies(): Result<List<Movie>> = Success(movies)
+    @Test
+    fun `moviesStateOutput on error returns failure`() {
+        val repository = StubMovieRepository()
+        repository.setErrorResult()
+
+        val viewModel = MoviesListViewModelImpl(repository, MoviesListItemMapper())
+
+        val state = viewModel.moviesStateOutput.value
+        assertTrue(
+            state is MoviesListViewState.FailedToLoad,
+            "state is $state"
+        )
+    }
+
+    internal class StubMovieRepository : MovieRepository {
+
+        private var result: Result<List<Movie>> = Success(emptyList())
+
+        fun setResult(movies: List<Movie>) {
+            result = Success(movies)
+        }
+
+        fun setErrorResult() {
+            result = Failure(Throwable())
+        }
+
+        override suspend fun loadMovies(): Result<List<Movie>> = result
         override suspend fun loadMovie(movieId: Int): Result<MovieDetails> =
             TODO("repository doesn't follow Interface Segregation Principle")
     }
