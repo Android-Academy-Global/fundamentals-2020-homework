01932c80f57206ffc750812f81a74a94c7df2226
Yurii Anisimov <31529762+an-yurii@users.noreply.github.com>
2021-09-29 19:37:09 +0700

    Workshop 3. Рефакторинг тестов. Создание тестов копированием.

--- app/src/test/java/com/android/academy/fundamentals/homework/presentation/features/movies/viewmodel/MoviesListViewModelImplTest.kt
+++ app/src/test/java/com/android/academy/fundamentals/homework/presentation/features/movies/viewmodel/MoviesListViewModelImplTest.kt
@@ -1,8 +1,10 @@
 package com.android.academy.fundamentals.homework.presentation.features.movies.viewmodel
 
+import com.android.academy.fundamentals.homework.common.model.Failure
 import com.android.academy.fundamentals.homework.common.model.Result
 import com.android.academy.fundamentals.homework.common.model.Success
 import com.android.academy.fundamentals.homework.domain.MovieRepository
+import com.android.academy.fundamentals.homework.model.Genre
 import com.android.academy.fundamentals.homework.model.Movie
 import com.android.academy.fundamentals.homework.model.MovieDetails
 import com.android.academy.fundamentals.homework.utils.viewModelTestingRules
@@ -18,7 +20,7 @@ class MoviesListViewModelImplTest {
     @Test
     fun `moviesStateOutput by default converts movie to movie list item`() {
         val movies = listOf(
-            Movie(
+            createMovie(
                 id = 671039,
                 title = "Bronx",
                 reviewCount = 200,
@@ -30,7 +32,8 @@ class MoviesListViewModelImplTest {
                 imageUrl = "test url"
             )
         )
-        val repository: MovieRepository = StubMovieRepository(movies = movies)
+        val repository: MovieRepository = StubMovieRepository()
+            .apply { setResult(movies) }
 
         val viewModel = MoviesListViewModelImpl(repository)
 
@@ -51,10 +54,71 @@ class MoviesListViewModelImplTest {
             )
     }
 
-    internal class StubMovieRepository(
-        val movies: List<Movie> = emptyList()
-    ) : MovieRepository {
-        override suspend fun loadMovies(): Result<List<Movie>> = Success(movies)
+    @Test
+    fun `moviesStateOutput by default return list in the same order`() {
+        val movies = listOf(
+            createMovie(id = 671),
+            createMovie(id = 1230),
+            createMovie(id = 54),
+        )
+        val repository: MovieRepository = StubMovieRepository()
+            .apply { setResult(movies) }
+
+        val viewModel = MoviesListViewModelImpl(repository)
+
+        val state = viewModel.moviesStateOutput.value
+        assertThat((state as MoviesListViewState.MoviesLoaded).movies.map { it.id })
+            .containsExactly (671, 1230, 54)
+    }
+
+    @Test
+    fun `moviesStateOutput in case of error returns failure`() {
+        val repository = StubMovieRepository()
+        repository.setErrorResult()
+
+        val viewModel = MoviesListViewModelImpl(repository)
+
+        val state = viewModel.moviesStateOutput.value
+        assertThat(state).isInstanceOf(MoviesListViewState.FailedToLoad::class.java)
+    }
+
+    private fun createMovie(
+        id: Int = 0,
+        title: String = "",
+        reviewCount: Int = 0,
+        isLiked: Boolean = false,
+        rating: Int = 0,
+        pgAge: Int = 0,
+        genres: List<Genre> = emptyList(),
+        runningTime: Int = 0,
+        imageUrl: String? = null,
+    ): Movie {
+        return Movie(
+            id = id,
+            title = title,
+            reviewCount = reviewCount,
+            isLiked = isLiked,
+            rating = rating,
+            pgAge = pgAge,
+            genres = genres,
+            runningTime = runningTime,
+            imageUrl = imageUrl
+        )
+    }
+
+    internal class StubMovieRepository : MovieRepository {
+
+        private var result: Result<List<Movie>> = Success(emptyList())
+
+        fun setResult(movies: List<Movie>) {
+            result = Success(movies)
+        }
+
+        fun setErrorResult() {
+            result = Failure(Throwable())
+        }
+
+        override suspend fun loadMovies(): Result<List<Movie>> = result
         override suspend fun loadMovie(movieId: Int): Result<MovieDetails> =
             TODO("repository doesn't follow Interface Segregation Principle")
     }
